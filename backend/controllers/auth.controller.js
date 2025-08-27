6ed4dcc7a73bfe2b976a0d6b7d83d70dfa01f254
jon1267 <jonleev@gmail.com>
2025-08-27 09:35:12 +0300

    auth controller, create user

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,19 @@
+import { json } from 'express';
+import { User } from '../models/user.model.js';
 export const signup = async (req, res) => {
+  const {email, password, name} = req.body;
+  try {
+    if (!email || !password || !name) {
+      throw new Error("All fields are required");
+    }
+    const userAlreadyExists = await User.findOne({email});    
+    if ( userAlreadyExists) {
+      return res.status(400).json({success: false, message: "user already exists"});
+    }    
+  } catch (error) {
+
+  }
+
   res.send('signup route');
 };
 
