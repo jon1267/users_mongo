7c0eaa6f965a5c5963700dc2220d16a0f243d7a5
jon1267 <jonleev@gmail.com>
2025-08-28 12:27:17 +0300

    create user in mongo cloud && fixes

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import { json } from 'express';
 import { User } from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+
 export const signup = async (req, res) => {
   const {email, password, name} = req.body;
   try {
@@ -8,10 +11,36 @@ export const signup = async (req, res) => {
     }
     const userAlreadyExists = await User.findOne({email});    
     if ( userAlreadyExists) {
-      return res.status(400).json({success: false, message: "user already exists"});
-    }    
-  } catch (error) {
+      return res.status(400).json({success: false, message: "User already exists"});
+    }
 
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verificationToken =  Math.floor(100000 + Math.random() * 900000).toString();
+
+    const user = new User({
+      email, 
+      password: hashedPassword, 
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000 // 24 hours
+    });
+
+    await user.save();
+
+    // jwt && Cookie
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: 'User created successfully',
+      user: {
+        ...user._doc,
+        password: undefined
+      }
+    });
+
+  } catch (error) {
+    return res.status(400).json({success: false, message: error.message});
   }
 
   res.send('signup route');
